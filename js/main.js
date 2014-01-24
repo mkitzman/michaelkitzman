1cc7cba2a94e66c66c5ad73e308021ba6c57f559
Michael Kitzman <dunkfudragon@yahoo.com>
2014-01-23 20:59:32 -0800

    initial check in

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//Ensure Main Content takes entire screen
+$(document).ready(function(){
+    resizeDiv();
+    showCard();
+});
+
+window.onresize = function(event) {
+    resizeDiv();
+}
+
+function resizeDiv() {
+    console.log('resizing');
+
+    vpw = $(window).width();
+    vph = $(window).height();
+
+    $('.screen-size').css({'height': vph + 'px'});
+}
+
+function showCard() {
+
+    $('.profile-card').addClass("show");
+
+}
+
+// $(window).scroll(function () {
+//     $('.profile-card').each(function () {
+
+//         if (($(this).offset().top - $(window).scrollTop()) < 20) {
+//             $(this).stop().fadeTo(900, 0);
+//         } else {
+//             $(this).stop().fadeTo('fast', 1);
+//         }
+//     });
+// });
