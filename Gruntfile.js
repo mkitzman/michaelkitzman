098449d797f1ccd32f6df8103071d68057737a5c
Michael Kitzman <dunkfudragon@yahoo.com>
2014-03-26 22:03:20 -0700

    add in grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+        yslow: {
+            options: {
+                thresholds: {
+                    weight: 180,
+                    speed: 1500,
+                    score: 80,
+                    requests: 15
+                }
+            },
+            pages: {
+                files: [
+                    {
+                        src: 'http://michaelkitzman.com'
+                    }
+                ]
+            }
+        },
+        concat: {
+            dist: {
+                src: [
+                    'js/vender/*.js', // All JS in the vender folder
+                    'js/main.js'  // This specific file
+                ],  
+                dest: 'js/build/production.js'
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'img/',
+                    src: ['**/*.{png,jpg,gif,jpeg}'],
+                    dest: 'img/build/'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-yslow');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    // Default task(s).
+    //grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'yslow']);
+    grunt.registerTask('default', ['concat', 'uglify', 'yslow']);
+
+};
