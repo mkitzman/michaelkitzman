5e578f05c8718d373b660da24ba444cfd3bc1954
Michael Kitzman <dunkfudragon@yahoo.com>
2015-01-03 18:52:03 -0800

    first checkin for v2 version of site

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,92 @@
 module.exports = function(grunt) {
-
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            build: {
-                src: 'js/build/production.js',
-                dest: 'js/build/production.min.js'
-            }
-        },
-        yslow: {
-            options: {
-                thresholds: {
-                    weight: 180,
-                    speed: 1500,
-                    score: 80,
-                    requests: 15
-                }
-            },
-            pages: {
-                files: [
-                    {
-                        src: 'http://michaelkitzman.com'
-                    }
-                ]
-            }
-        },
+
         concat: {
             dist: {
                 src: [
-                    'js/vender/*.js', // All JS in the vender folder
-                    'js/main.js'  // This specific file
-                ],  
-                dest: 'js/build/production.js'
+                    'assets/js/*.js'
+
+                ],
+                dest: 'assets/dist/<%= pkg.name %>.js'
             }
         },
-        phantomas: {
-            gruntSite : {
-                options : {
-                    indexPath : './phantomas/',
-                    options   : {},
-                    url       : 'http://michaelkitzman.com/'
+
+        cssmin: {
+            target: {
+                files: {
+                    'assets/dist/<%= pkg.name %>.css': ['assets/css/bootstrap.css', 'assets/css/site.css']
                 }
             }
         },
+
         imagemin: {
             dynamic: {
                 files: [{
                     expand: true,
-                    cwd: 'img/',
-                    src: ['**/*.{png,jpg,gif,jpeg}'],
-                    dest: 'img/build/'
+                    cwd: 'assets/img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'assets/dist/'
                 }]
+            }
+        },
+
+        uglify: {
+            options: {
+              banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'assets/js/<%= pkg.name %>.js',
+                dest: 'assets/dist/<%= pkg.name %>.min.js'
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'assets/css/bootstrap.css': 'assets/scss/bootstrap.scss',
+                    'assets/css/site.css': 'assets/scss/site.scss'
+                }
+            } 
+        },
+
+
+        watch: {
+            scripts: {
+                files: ['assets/js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            },
+
+            css: {
+                files: ['scss/*.scss'],
+                tasks: ['sass', 'autoprefixer'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-yslow');
+    // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-phantomas');
-    //grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
 
-    // Default task(s).
-    //grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'yslow']);
-    grunt.registerTask('default', ['phantomas', 'concat', 'uglify', 'yslow']);
+    grunt.registerTask('default', ['imagemin', 'sass', 'cssmin', 'concat', 'uglify']);
+    //grunt.registerTask('dev', ['watch']);
 
 };
+
