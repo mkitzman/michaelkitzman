71b25d6204cb3b1481fd8060e32f83e4576be187
Michael Kitzman <dunkfudragon@yahoo.com>
2015-01-04 21:28:18 -0800

    visual updates

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,8 +1,17 @@
 (function(){
     "use strict";
     
-    var profile_card_node = document.getElementById('profile-card');
-    console.log(profile_card_node);
-    
-    profile_card_node.classList.add('show');
+    //Fade in elements on page load
+    var fadeable_elements = document.getElementsByClassName('fadeable'),
+        data_attribute;
+
+    for (var i = 0; i < fadeable_elements.length; ++i) {
+
+        data_attribute = fadeable_elements[i].getAttribute('data-img-src');
+        
+        if(data_attribute) {
+            fadeable_elements[i].setAttribute("src", data_attribute);
+        }
+        fadeable_elements[i].classList.add('show');
+    }
 }());
