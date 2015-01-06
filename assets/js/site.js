ef7678f8c78418d6559ab446f574bcb1009b41a7
Michael Kitzman <dunkfudragon@yahoo.com>
2015-01-05 21:48:02 -0800

    add js for transistions, html updates, jquery

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,17 +1,46 @@
-(function(){
-    "use strict";
-    
-    //Fade in elements on page load
-    var fadeable_elements = document.getElementsByClassName('fadeable'),
-        data_attribute;
+$( document ).ready(function() {
 
-    for (var i = 0; i < fadeable_elements.length; ++i) {
-
-        data_attribute = fadeable_elements[i].getAttribute('data-img-src');
+    (function(){
+        "use strict";
         
-        if(data_attribute) {
-            fadeable_elements[i].setAttribute("src", data_attribute);
+        //Fade in elements on page load
+        var fadeable_elements = document.getElementsByClassName('fadein-onload'),
+            data_attribute;
+
+        for (var i = 0; i < fadeable_elements.length; ++i) {
+
+            data_attribute = fadeable_elements[i].getAttribute('data-img-src');
+
+            fadeable_elements[i].classList.add('show');
+
+            if(data_attribute) {
+                fadeable_elements[i].setAttribute("src", data_attribute);
+            }
+            
         }
-        fadeable_elements[i].classList.add('show');
-    }
-}());
+    }());
+
+
+    var wrap = $(".avatar");
+    var navbar = $(".navbar");
+    var height;
+
+    $(window).scroll(function() {
+        
+        height = $(window).scrollTop();
+        console.log(height);
+        if (height > 150) { 
+            navbar.addClass("show");
+        } else {
+            navbar.removeClass("show");
+        }
+
+        if (height > 275) {
+            wrap.addClass("static");
+        } else {
+            wrap.removeClass("static");
+        }
+
+    });
+});
+
